b88ad7ffbd98cd9425ad12f7b7121650531f74ea
Rida <hamdanmadaraa@gmail.com>
2025-03-03 04:23:40 +0500

    change production URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: '' // Replace with production API URL when deploying
+    apiUrl: 'https://express-currency-converter-a2z8.vercel.app/' // Replace with production API URL when deploying
   };
